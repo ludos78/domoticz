6ef425cd0e103e635d79760ee41ac69a149ac59c
Ludos78 <99007852+ludos78@users.noreply.github.com>
2024-10-16 22:39:24 +0200

    use metter / wattmeter

--- hardware/EnOceanESP3.cpp
+++ hardware/EnOceanESP3.cpp
@@ -3212,39 +3212,15 @@ void CEnOceanESP3::ParseERP1Packet(uint8_t *data, uint16_t datalen, uint8_t *opt
 					uint8_t DT = bitrange(DATA_BYTE0, 2, 0x01); // 0 = cumulative count, 1 = current value / s
 					uint8_t DIV = bitrange(DATA_BYTE0, 0, 0x03);
 					float scaleMax = (DIV == 0) ? 16777215.000F : ((DIV == 1) ? 1677721.500F : ((DIV == 2) ? 167772.150F : 16777.215F));
-					uint32_t MR = ground(GetDeviceValue((DATA_BYTE3 << 16) | (DATA_BYTE2 << 8) | DATA_BYTE1, 0, 16777215, 0.0F, scaleMax));
+					float MR = GetDeviceValue((DATA_BYTE3 << 16) | (DATA_BYTE2 << 8) | DATA_BYTE1, 0, 16777215, 0.0F, scaleMax);
 
 					if (DT == 0)
 					{ // comulated count
-						RBUF tsen;
-						memset(&tsen, 0, sizeof(RBUF));
-						tsen.RFXMETER.packetlength = sizeof(tsen.RFXMETER) - 1;
-						tsen.RFXMETER.packettype = pTypeRFXMeter;
-						tsen.RFXMETER.subtype = sTypeRFXMeterCount;
-						tsen.RFXMETER.seqnbr = 0;
-						tsen.RFXMETER.id1 = ID_BYTE2;
-						tsen.RFXMETER.id2 = ID_BYTE1;
-						tsen.RFXMETER.count1 = (BYTE) ((MR & 0xFF000000) >> 24);
-						tsen.RFXMETER.count2 = (BYTE) ((MR & 0x00FF0000) >> 16);
-						tsen.RFXMETER.count3 = (BYTE) ((MR & 0x0000FF00) >> 8);
-						tsen.RFXMETER.count4 = (BYTE) (MR & 0x000000FF);
-						// WARNING
-						// filler field is used here to transmit CH value to decode_RFXMeter in mainworker.cpp
-						tsen.RFXMETER.filler = CH;
-						tsen.RFXMETER.rssi = rssi;
-						sDecodeRXMessage(this, (const unsigned char *) &tsen.RFXMETER, pNode->name.c_str(), -1, m_Name.c_str());
+						SendKwhMeter(senderID, CH, -1, 0.0, MR, pNode->name.c_str(), rssi);
 					}
 					else
 					{ // instant value
-						_tUsageMeter umeter;
-						umeter.id1 = (BYTE) ID_BYTE3;
-						umeter.id2 = (BYTE) ID_BYTE2;
-						umeter.id3 = (BYTE) ID_BYTE1;
-						umeter.id4 = (BYTE) ID_BYTE0;
-						umeter.dunit = CH;
-						umeter.fusage = MR;
-						umeter.rssi = rssi;
-						sDecodeRXMessage(this, (const unsigned char *) &umeter, pNode->name.c_str(), -1, m_Name.c_str());
+						SendWattMeter(senderID, CH, -1, MR, pNode->name.c_str(), rssi);
 					}
 
 					Debug(DEBUG_NORM, "4BS msg: Node %08X (%s) CH %u DT %u DIV %u (scaleMax %.3F) MR %u",
